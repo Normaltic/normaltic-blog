eb0228bc670b8e84fe8ddaf127607852fee8b875
yj.kim <normaltic@gmail.com>
2020-09-09 13:23:42 +0900

    Post 내부 디자인 수정

--- src/components/post.js
+++ src/components/post.js
@@ -5,19 +5,66 @@ import styled from 'styled-components';
 import Layout from './layout';
 import SEO from './seo';
 
+const COLOR = {
+  PRIMARY: '#009fe7',
+  LIGHTGRAY: '#f5f5f5',
+  DARKGRAY: '#5c5c5c',
+};
+
 const Head = styled.div`
   margin-bottom: 4rem;
   h1 {
     font-size: 2rem;
   }
-`
+`;
 
 const Content = styled.div`
-  h2 {
-    padding-left: 0.5rem;
-    border-left: 0.2rem solid black;
+  padding-right: 10%;
+  @media screen and ( max-width: 960px ) {
+    padding-right: initial;
   }
-`
+
+  // ###
+  h3 {
+    margin-top: 4rem;
+  }
+
+  // ~~
+  del {
+    font-size: 0.9rem;
+    font-style: italic;
+    color: ${COLOR.DARKGRAY};
+  }
+
+  // (link)[url]
+  a {
+    color: ${COLOR.PRIMARY};
+  }
+
+  // >
+  blockquote {
+    margin: 2rem 0;
+    padding: 1.5rem;
+    border-left: 0.2rem solid ${COLOR.PRIMARY};
+    background-color: ${COLOR.LIGHTGRAY};
+  }
+
+  // image
+  figure[class='gatsby-resp-image-figure'] {
+    margin-bottom: 2.45rem;
+    figcaption {
+      margin-top: 0.5rem;
+      text-align: center;
+      color: ${COLOR.DARKGRAY};
+      font-style: italic;
+    }
+  }
+
+  // code
+  pre {
+    margin-bottom: 2.45rem;
+  }
+`;
 
 const Template = ({pageContext}) => {
   const {title, html, date} = pageContext;
